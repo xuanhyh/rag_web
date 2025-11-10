63a54019764e2fb30a1f81610ad0c43bfe290db7
suidyl <1826514573@qq.com>
2025-11-10 19:09:23 +0800

    修改了登录路径

--- start_web.py
+++ start_web.py
@@ -6,7 +6,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "api:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=8000,
         reload=True,
         log_level="info"
